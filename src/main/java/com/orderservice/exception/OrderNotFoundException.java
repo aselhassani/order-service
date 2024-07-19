a85402c4e61eed76c398f65e9b26918804e0a4de
Ali SAIDI ELHASSANI <saidi.elhassani@gmail.com>
2024-07-19 08:44:00 +0100

    code formatting

--- src/main/java/com/orderservice/exception/OrderNotFoundException.java
+++ src/main/java/com/orderservice/exception/OrderNotFoundException.java
@@ -6,9 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class OrderNotFoundException extends RuntimeException {
-
   private final String orderNumber;
-
   @Override
   public String getMessage() {
     return String.format("No order with number '%s' was found", orderNumber);
--- src/main/java/com/orderservice/exception/UpdateNotAllowedException.java
+++ src/main/java/com/orderservice/exception/UpdateNotAllowedException.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class UpdateNotAllowedException extends RuntimeException {
-
   @Override
   public String getMessage() {
     return "Update not allowed more than 5 minutes after the creation";
